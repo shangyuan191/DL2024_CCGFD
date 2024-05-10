1193a6055dd14ff2e1ede4a00b16bf84a6e77e63
marco310 <limarco310@gmail.com>
2024-05-10 23:55:35 +0800

    test_tyl3000

---
+++ tyl3000/base.py
@@ -0,0 +1,14 @@
+from torch import nn
+
+class tab_model(nn.Module):
+    def __init__(self):
+        super(tab_model, self).__init__()
+        pass
+
+    def forward(self, x, A=None):
+        #TODO:
+        #   1:glob model
+        #   2:DGM(GSL)
+        #   3:Downstream model
+
+        pass
