de92b708f70ca93f2c03ee7a4c9cc6ff2f2eae96
marco310 <limarco310@gmail.com>
2024-05-11 19:59:46 +0800

    done attention_20240511_1710_tyl3000

--- tyl3000/src/modules/globa.py
+++ tyl3000/src/modules/globa.py
@@ -1,18 +1,15 @@
 
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 
-
-class _attention(nn.Module):
-    def __init__(self, in_channels, out_channels, num_heads, output_attn=False, is_causal=False, feed_forward=nn.Linear, use_weight=True):
+class base_attention(nn.Module):
+    def __init__(self, in_channels, out_channels, num_heads, output_attn=True, feed_forward=nn.Linear, use_weight=True):
         super().__init__()
         self.in_channels = in_channels
         self.out_channels = out_channels
         self.num_heads = num_heads
 
         self.output_attn = output_attn
-        self.is_causal = is_causal
         self.feed_forward = feed_forward
         self.use_weight = use_weight
 
@@ -40,11 +37,12 @@ class _attention(nn.Module):
         if self.use_weight:
             self.Wv.reset_parameters()
 
-class simple_attention(_attention):
-    def max_mean_attention(self):
-        pass
-    def mean_attention(self):
-        pass
+
+class simple_attention(base_attention):
+    """
+    SGFormer attention:
+        https://github.com/qitianwu/SGFormer/blob/main/large/ours.py
+    """
     def simple_attention(self, qs, ks, vs):
         qs = qs / torch.norm(qs, p=2)
         ks = ks / torch.norm(ks, p=2)
@@ -67,12 +65,8 @@ class simple_attention(_attention):
         attn_output = attention_num / attention_normalizer  # [N, H, D]
 
         # compute attention for visualization if needed
-        # this not work yet
         if self.output_attn:
-            attention = torch.einsum("nhm,lhm->nlh", qs, ks).mean(dim=-1)
-            normalizer = attention_normalizer.squeeze(dim=-1).mean(dim=-1, keepdims=True)
-            attention = attention / normalizer
-            #attention = torch.einsum("nhm,lhm->nlh", qs, ks).mean(dim=2) / attention_normalizer.mean(dim=1).reshape(-1)  # [N, L, H]
+            attention = torch.einsum("nhm,lhm->nlh", qs, ks).mean(dim=-1) / attention_normalizer.squeeze(dim=-1).mean(dim=-1)  # [N, L, H]
             return attn_output, attention
 
         return attn_output
@@ -86,19 +80,24 @@ class simple_attention(_attention):
         return out
 
 
+class torch_attention(base_attention):
+    def __init__(self, in_channels, out_channels, num_heads, output_attn=True, dropout_p=0.0, feed_forward=nn.Linear, use_weight=True):
+        super(torch_attention, self).__init__(in_channels, out_channels, num_heads, output_attn, feed_forward, use_weight)
+        self.dropout_p = dropout_p
+        #windows: xformers
+        #Linux : Flash Attention
+        self.MHA = nn.MultiheadAttention(out_channels, num_heads)
 
-
-class flash_attention(_attention):
     def forward(self, query_input, source_input):
         qs, ks, vs = self._input_feed_forward(query_input, source_input)
-        #out = self.simple_attention(qs, ks, vs)
-        #out = out.mean(dim=1)
-        #return out
-
+        out, att = self.MHA(qs, ks, vs)
+        if self.output_attn:
+            return out, att.mean(dim=0)
+        return out
 
 
 if __name__ == '__main__':
-    batch = 8
+    batch = 16
     i_dim = 512
     o_dim = 128
     head = 4
@@ -107,7 +106,10 @@ if __name__ == '__main__':
     simple_model = simple_attention(i_dim, o_dim, head, True).to("cuda")
 
     simple_y = simple_model(x, x)
-    print(simple_y[1].shape)
+    print(simple_y[0].shape, simple_y[1].shape)
 
-#def flash_attention(qs, ks, vs, output_attn=False):
+    torch_model = torch_attention(i_dim, o_dim, head, True, 0.0).to("cuda")
+    torch_y = torch_model(x, x)
+    print(torch_y[0].shape, torch_y[1].shape)
+
 
