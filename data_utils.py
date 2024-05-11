ea321d707e8e136a5b5715755ee2078382ae8195
leeeating <nike890825@gamil.com>
2024-05-11 19:01:34 +0800

    init commit

---
+++ data_utils.py
@@ -0,0 +1,52 @@
+import numpy as np
+import torch
+import os.path as osp
+
+from torch_geometric.data import Data, InMemoryDataset, download_url
+
+class CreditCard(InMemoryDataset):
+
+    def __init__(self, root, cos, transform=None, pre_transform=None):
+
+        self.name = f'creaditcard_cos{int(cos*10)}'
+        self.cos = cos
+
+        super().__init__(root, transform, pre_transform)
+        
+        self.load(self.processed_paths[0])
+
+
+    @property
+    def raw_dir(self) -> str:
+        return osp.join(self.root, 'raw')
+
+    @property
+    def processed_dir(self) -> str:
+        return osp.join(self.root, self.name, 'processed')
+
+    @property
+    def raw_file_names(self):
+        return ['creditcard.npz']
+
+    @property
+    def processed_file_names(self):
+        return ['data.pt']
+    
+    def download(self):
+        pass
+
+    def process(self):
+        
+        data = np.load(self.raw_paths[0])
+
+        x = data['features']
+        y = data['label']
+        cos_sim = data['cosine_sim']
+
+        mask = cos_sim > self.cos
+        src, dst = np.nonzero(mask)
+        edge_index = np.stack([src, dst])
+
+
+        data = Data(x=x, y=y, edge_index=edge_index)
+        self.save([data], self.processed_paths[0])
